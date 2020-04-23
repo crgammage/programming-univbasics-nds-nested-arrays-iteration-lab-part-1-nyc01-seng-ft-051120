54257cdf240730f07d25aeb1d86ca4bac9013e9b
Christa Gammage <crgammage@gmail.com>
2020-04-23 19:25:55 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,7 +2,7 @@ def find_even_values(array_of_arrays)
   row_index = 0
   while row_index < array_of_arrays.count do
     element_index = 0
-    while element_index < array_of_arrays[row_index].count
+    while element_index < array_of_arrays[row_index].count do
       if array_of_arrays[row_index][element_index].even?
         p array_of_arrays[row_index][element_index]
         element_index += 1
